5ad5efd481b1315ed32683a04dcd81cbc85771ff
ximo <ximo.coder@gmail.com>
2016-07-09 16:56:08 +0200

    Added textcontroller and first text change

---
+++ Assets/TextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+	// Use this for initialization
+	void Start () {
+                
+        text.text = "Erase una vez en la mancha...";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
