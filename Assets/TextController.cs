b5e2d1035e467ef5e4553b1fdad8c030dbf17f76
ximo <ximo.coder@gmail.com>
2016-07-09 21:58:33 +0200

    webgl build and web build

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -3,18 +3,25 @@ using UnityEngine.UI;
 using System.Collections;
 
 
-public class TextController : MonoBehaviour {
+public class TextController : MonoBehaviour
+{
 
     public Text text;
-	// Use this for initialization
-	void Start () {
-                
-        text.text = "Erase una vez en la mancha...";
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        text.text = "Shur? eres tu? Pulsa espacio...";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = "DEP";
+        }
+
+    }
 }
